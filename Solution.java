26b1c59b4aec2f1f69fc9d9aab4bfe45280bff76
Ahamed Faizal A <ahamedfaizal51122@gmail.com>
2026-05-25 10:47:45 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        //string
+        int n = s.length();
+        int ans = 0;
+        int[] index = new int[128]; 
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String s = "abcabcbb";
+        int result = sol.lengthOfLongestSubstring(s);
+        System.out.println("Length of the longest substring without repeating characters: " + result);
+    }
+}
